8a3b32b1184877ea0b618adbb3fbd086147ef5ca
anil sidhu <webanilsidhu@gmail.com>
2023-06-06 21:44:30 +0530

    basic routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,9 @@
-'use client'
-import { useState } from 'react'
 import styles from './page.module.css'
 
-
 export default function Home() {
-  const [name, setName] = useState("Anil")
-
-  const apple = () => {
-    setName("Sidhu")
-  }
-
-  const InnerComp = () => {
-    return (
-      <h1>Inner Component</h1>
-    )
-  }
   return (
     <main className={styles.main}>
-      <h1>Events , function and state {name} </h1>
-      <button onClick={() => apple()}>Click Me</button>
-      {/* <InnerComp /> */}
-      {InnerComp()}
+     <h1>Basic Routing | Make New Page</h1> 
     </main>
   )
 }
