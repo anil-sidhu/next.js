e628d932fa0a7aabb47afe02192d2e118c8fe096
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-06-13 00:09:50 +0530

    done

---
+++ src/app/study/[...lecture]/page.js
@@ -0,0 +1,11 @@
+"use client"
+export default function Lecture({params}){
+    console.log(params);
+    return(
+        <div>
+            <h1> Day of college {params.lecture[0]} </h1>
+            <h2>Lecture No. {params.lecture[1]} </h2>
+
+        </div>
+    )
+}
---
+++ src/app/study/page.js
@@ -0,0 +1,7 @@
+export default function Study(){
+    return(
+        <div>
+            <h1>All Lectures of College</h1>
+        </div>
+    )
+}
