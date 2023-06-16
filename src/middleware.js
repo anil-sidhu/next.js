1479fc4d2a82cd3e397be4c942cb70bd19a4c24c
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-06-16 20:53:42 +0530

    Done

---
+++ src/middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request){
+
+    // if(request.nextUrl.pathname!="/login"){
+     return NextResponse.redirect(new URL("/login",request.url))
+    // }
+
+}
+
+export const config={
+    matcher:["/about/:path*", "/studentlist/:path*"]
+}
