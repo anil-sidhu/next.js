481da19c446225b50a1b8539da11d1eb048980c2
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-06-27 20:17:57 +0530

    done with css

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,22 @@
 "use client"
-
-import Link from "next/link"
-
-
+import { useState } from 'react'
+import style from './style.module.css'
 export default function Home() {
-
+  const [color,setColor]=useState("red");
+  const {red} = style
   return (
     <main>
-      <h1>Fetch Data with API in Client Component </h1>
-      <Link href="/productlist">Go to Product List</Link>
+      <h1 className={color=='red'?style.red:style.green} >Condtion with Style </h1>
+      <h2 style={{backgroundColor:color=='red'?'red':'green'}} >Heading 2</h2>
+      <h3 id={style.orange} >Heading 3</h3>
+
+      <h4  className={red}>Dummy text</h4>
+      <h4 className={red}>Dummy text</h4>
+      <h4 className={red}>Dummy text</h4>
+      <h4 className={red}>Dummy text</h4>
+
+
+      <button onClick={()=>setColor("green")} >Update Color</button>
     </main>
   )
 }
