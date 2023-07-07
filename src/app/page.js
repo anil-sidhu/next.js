244fcf2c33c2d3fef49fce9eb55ad90c731c0d10
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2023-07-07 19:04:14 +0530

    Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
+"use client"
+import { API_BASE_URL } from "@/config/constants"
+
 export default function Home() {
 
+  console.log(process.env.NODE_ENV)
   return (
     <main>
+      {
+        process.env.NODE_ENV=="development"?
+        <h1>You are on development Mode</h1>
+        :
+        <h1>You are on Production Mode</h1>
+      }
       <h1>Environment Variables in Next js </h1>
+
+    <h1> {API_BASE_URL}</h1>
     </main>
   )
 }
---
+++ src/config/constants.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL= process.env.NODE_ENV=="development" ?"www.api.dev.example.com":"www.api.prod.example.com"
